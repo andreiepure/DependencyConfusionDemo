727789af53cf926783485017d848c39eabb86498
Andrei Epure (personal) <epure.andrei@gmail.com>
2022-08-09 21:52:24 +0200

    Update the Corporate App naming

--- CorporateApp/Program.cs
+++ CorporateApp/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Corporate.Private.Library;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine($"Hello, Corporation! I can use a safe, private {new CorporateClass()}");
