1e952327dd7d8534d1dcd0e192d07af8517edbcb
Andrei Epure (personal) <epure.andrei@gmail.com>
2023-05-15 19:32:54 +0200

    rm build noise;

--- CorporateApp/Corporate.Private.App.Typo/Program.cs
+++ CorporateApp/Corporate.Private.App.Typo/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Google.Protobuf;
 using System;
 
-Console.WriteLine("Hello, Corporation! I will use Google.Protobuf at some point");
+Console.WriteLine($"Hello, Corporation! I will use Google.Protobuf at some point in {PrintMessage}");
 
 void PrintMessage(IMessage message)
 {
